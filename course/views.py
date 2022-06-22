d31f3e63d05d4dccb0c43949475a2b7aeb6e8bfa
faltunik <niksanat8@gmail.com>
2022-06-22 19:01:35 +0530

    refactored code

--- course/views.py
+++ course/views.py
@@ -6,14 +6,6 @@ from rest_framework.views import APIView
 from .models import Course
 from .serializer import CourseSerializer,  StudentListSerializer
 
-# Create your views here.
-
-
-
-
-# API to create course
-# API to list all course
-# API to retrieve a course
 class CourseAPI(viewsets.ModelViewSet):
     def create(self, request):
         if request.user.teacher:
@@ -26,19 +18,12 @@ class CourseAPI(viewsets.ModelViewSet):
         return Response({"error": "You Dont Have Permission"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-
-# API Enroll on Course
-
 class Enrollment(APIView):
-    def get(self, request):
+    def enroll_user(self, request):
         obj = get_object_or_404(Course, id = request.GET.get('getid', 1) )
         obj.students.add(request.user)
         return Response({"Enrolled": "You Are Enrolled"}, status=status.HTTP_202_ACCEPTED )
 
-
-
-
-# API to get list of all students
 class StudentList(APIView):
     def get(self, request):
         obj = get_object_or_404(Course, id = request.GET.get('getid', 1) )
